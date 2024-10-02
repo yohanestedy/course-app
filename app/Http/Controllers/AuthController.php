e0b68846ac56c04783590e983a69f1ff3cc7cf12
kwul0208 <kwul0208@gmail.com>
2024-10-02 20:34:39 +0700

    day 46 middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
@@ -52,26 +53,41 @@ class AuthController extends Controller
             return Redirect::back()->withErrors($validator)->withInput();
         }
 
+        $credential = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        // STEP 1
         // check apakah email yg diinputkan ada usernya
-        $user = User::where('email', $request->email)->first();
-        // kemungkinan output user = {.. data} / null
-        if(!$user){
-            // return 'email empty'. $request->email;
-            return redirect()->back()->with('error', 'email or password not valid');
-        }
+        // $user = User::where('email', $request->email)->first();
+        // // kemungkinan output user = {.. data} / null
+        // if(!$user){
+        //     // return 'email empty'. $request->email;
+        //     return redirect()->back()->with('error', 'email or password not valid');
+        // }
         
+        // STEP 2
         // check apakah passwordnya ini sama dengan usernya
         // Auth::attempt($post)
         // !Hash::check($fields['password'], $user->password)
-        if (!Hash::check($request->password, $user->password)) {
-            // return 'password wrong'. $request->password;
-            return redirect()->back()->with('error', 'email or password not valid');
+        // if (!Hash::check($request->password, $user->password)) {
+        //     // return 'password wrong'. $request->password;
+        //     return redirect()->back()->with('error', 'email or password not valid');
+        // }
+
+        // STEP 3
+        if(Auth::attempt($credential)){
+            $request->session()->regenerate();
+            return redirect()->intended('/')->with('success', 'success login');
         }
 
-        $request->session()->regenerate();
-        return redirect()->intended('/')->with('success', 'success login');
-
         // redirect ke home page
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect('/login')->with(['success' => 'seccess logout']);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.master.index');
-});
+
 
 Route::get('/hello-world', [HelloController::class, 'index']);
 
@@ -30,46 +28,60 @@ Route::prefix('/blog')->group(function () {
     Route::get('/show/{slug}', [BlogController::class, 'show']);
 });
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/register/store', [AuthController::class, 'storeRegister'])->name('register.store');
-Route::post('/login/store', [AuthController::class, 'storeLogin'])->name('login.store');
+Route::group([
+    'middleware' => 'guest'
+], function(){
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register/store', [AuthController::class, 'storeRegister'])->name('register.store');
+    Route::post('/login/store', [AuthController::class, 'storeLogin'])->name('login.store');
+});
 
 
 //Mini Apps
+// hanya bisa diakses jika sudah login
+Route::group([
+    'middleware' => 'auth'
+], function(){
+    Route::get('/', function () {
+        return view('pages.master.index');
+    })->name('home');
+    // DUSUN
+    Route::prefix('dusun')->group(function () {
+        Route::get('/', [DusunController::class, 'index'])->name('dusun.index');
+        Route::get('/detail/{id}', [DusunController::class, 'detail'])->name('dusun.detail');
+    
+        // TAMBAH DUSUN
+        Route::get('/add', [DusunController::class, 'add'])->name('dusun.add');
+        Route::post('/store', [DusunController::class, 'store'])->name('dusun.store');
+    
+        // EDIT DUSUN
+        Route::get('/edit/{id}', [DusunController::class, 'edit'])->name('dusun.edit');
+        Route::put('/update/{id}', [DusunController::class, 'update'])->name('dusun.update');
+    
+    
+        // delete dusun
+        Route::delete('/delete/{id}', [DusunController::class, 'delete'])->name('dusun.delete');
+    });
+    
+    Route::prefix('warga')->group(function () {
+        // INDEX WARGA
+        Route::get('/', [WargaController::class, 'index'])->name('warga.index');
+        Route::get('/detail/{id}', [WargaController::class, 'detail'])->name('warga.detail');
+    
+        // TAMBAH WARGA
+        Route::get('/add', [WargaController::class, 'add'])->name('warga.add');
+        Route::post('/store', [WargaController::class, 'store'])->name('warga.store');
+    
+        // EDIT WARGA
+        Route::get('/edit/{id}', [WargaController::class, 'edit'])->name('warga.edit');
+        Route::put('/update/{id}', [WargaController::class, 'update'])->name('warga.update');
+    
+    
+        // delete WARGA
+        Route::delete('/delete/{id}', [WargaController::class, 'delete'])->name('warga.delete');
+    });
 
-// DUSUN
-Route::prefix('dusun')->group(function () {
-    Route::get('/', [DusunController::class, 'index'])->name('dusun.index');
-    Route::get('/detail/{id}', [DusunController::class, 'detail'])->name('dusun.detail');
-
-    // TAMBAH DUSUN
-    Route::get('/add', [DusunController::class, 'add'])->name('dusun.add');
-    Route::post('/store', [DusunController::class, 'store'])->name('dusun.store');
-
-    // EDIT DUSUN
-    Route::get('/edit/{id}', [DusunController::class, 'edit'])->name('dusun.edit');
-    Route::put('/update/{id}', [DusunController::class, 'update'])->name('dusun.update');
-
-
-    // delete dusun
-    Route::delete('/delete/{id}', [DusunController::class, 'delete'])->name('dusun.delete');
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::prefix('warga')->group(function () {
-    // INDEX WARGA
-    Route::get('/', [WargaController::class, 'index'])->name('warga.index');
-    Route::get('/detail/{id}', [WargaController::class, 'detail'])->name('warga.detail');
-
-    // TAMBAH WARGA
-    Route::get('/add', [WargaController::class, 'add'])->name('warga.add');
-    Route::post('/store', [WargaController::class, 'store'])->name('warga.store');
-
-    // EDIT WARGA
-    Route::get('/edit/{id}', [WargaController::class, 'edit'])->name('warga.edit');
-    Route::put('/update/{id}', [WargaController::class, 'update'])->name('warga.update');
-
-
-    // delete WARGA
-    Route::delete('/delete/{id}', [WargaController::class, 'delete'])->name('warga.delete');
-});
